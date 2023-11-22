f8b8260a3de603d83aa751ef1ab77360c33ab4bf
LautaroJLZ <Lautarojoseluis1234@gmail.com>
2023-11-22 17:52:28 -0300

    Add more settings for deploy

--- back/index.js
+++ back/index.js
@@ -1,26 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const app=express()
-const mongoose=require('mongoose')
-const rutas=require('./routes/routes')
+// require('dotenv').config()
+// const express = require('express')
+// const app=express()
+// const mongoose=require('mongoose')
+// const rutas=require('./routes/routes')
+// const cors = require('cors');
+// app.use(express.json())
+// const dbUrl = process.env.DB_URL
+// app.use(cors());
+// app.use('/miapi/pepe',rutas)
+// const PORT = process.env.PORT
+// mongoose.connect(dbUrl)
+// .then(()=>{
+//     console.log('se conecto la base')
+//     app.listen(process.env.PORT,()=>{
+//     console.log('el servidor esta listo en el puerto: ' + PORT)
+// })
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const app = express();
+const rutas = require('./routes/routes');
 
+app.use(express.json());
 
-app.use(express.json())
-const dbUrl = process.env.DB_URL
-
+// Configuración de CORS
 app.use(cors());
 
+const dbUrl = process.env.DB_URL;
+const PORT = process.env.PORT;
 
-app.use('/miapi/pepe',rutas)
-const PORT = process.env.PORT
+app.use('/miapi/pepe', rutas);
 
-mongoose.connect(dbUrl)
-.then(()=>{
-    console.log('se conecto la base')
-    app.listen(process.env.PORT,()=>{
-    console.log('el servidor esta listo en el puerto: ' + PORT)
-})
-}).catch((error)=>{
-    console.log(error)
-})
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Se conectó a la base de datos');
+    app.listen(PORT, () => {
+      console.log(`El servidor está listo en el puerto: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error al conectar con la base de datos:', error);
+  });
