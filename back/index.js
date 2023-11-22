68c64d78a120a57e729ba0216111458f5b84ef95
LautaroJLZ <Lautarojoseluis1234@gmail.com>
2023-11-22 14:01:15 -0300

    Agregamos variables de entorno

--- back/index.js
+++ back/index.js
@@ -1,19 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 
 const app=express()
 const mongoose=require('mongoose')
 const rutas=require('./routes/routes')
 
+
 app.use(express.json())
+const dbUrl = process.env.DB_URL
 
 
 app.use('/miapi/pepe',rutas)
+const PORT = process.env.PORT
 
-mongoose.connect('mongodb+srv://LautaroJLZ:mongoAtlas23*@cluster0.00gruig.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(dbUrl)
 .then(()=>{
     console.log('se conecto la base')
-    app.listen(3200,()=>{
-    console.log('el servidor esta listo')
+    app.listen(process.env.PORT,()=>{
+    console.log('el servidor esta listo en el puerto: ' + PORT)
 })
 }).catch((error)=>{
     console.log(error)
